19009a42aafac4c945c913679b12063d0fc3887f
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-07-31 12:02:46 +0300

    Create SolveMeFirst.kt

---
+++ SolveMeFirst.kt
@@ -0,0 +1,15 @@
+/**
+ * Complete the function solveMeFirst to compute the sum of two integers.
+ * */
+
+import java.util.*
+
+fun solveMeFirst(a: Int, b: Int): Int = a+b
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val num1 = sc.nextInt()
+    val num2 = sc.nextInt()
+    val sum = solveMeFirst(num1, num2)
+    println(sum)
+}
